812e4dde5af7e855fac3e7e84f59bff99bf0e741
lUANA lEOCADIO <luanaprinces13@gmail.com>
2025-06-03 00:32:08 +0100

    resolvendo conflitos

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -7,18 +7,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import modelo.Categoria;
 import modelo.Produto;
 
 /**
  * Realiza a persistência de dados.
  */
-public class AlunoDAO {
+public class ProdutoDAO {
 
-    //Utilizado para retornar uma lista de alunos.
+    //Utilizado para retornar uma lista de Produtos.
     public ArrayList<Produto> minhaLista = new ArrayList<>();
 
     /**
-     * Retorna a Lista de Alunos(objetos)
+     * Retorna a Lista de Produtos(objetos)
      */
     public ArrayList<Produto> getMinhaLista() {
 
@@ -26,17 +27,19 @@ public class AlunoDAO {
 
         try {
             Statement stmt = this.getConexao().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM tb_alunos");
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_produtos");
             while (res.next()) {
 
                 int id = res.getInt("id");
                 String nome = res.getString("nome");
-                int idade = res.getInt("idade");
-                String curso = res.getString("curso");
-                int fase = res.getInt("fase");
+                double preco = res.getDouble("preco");
+                String unidade = res.getString("unidade");
+                int quantidade = res.getInt("quantidade");
+                int quantidadeMinima = res.getInt("quantidadeMinima");
+                int quantidadeMaxima = res.getInt("quantidadeMaxima");
+                Categoria categoria = res.getInt("categoria");
 
-                Produto objeto = new Produto(id, nome, idade, curso, fase);
-
+                Produto objeto = new Produto(id, nome, preco, unidade, quantidade, quantidadeMinima, quantidadeMaxima, categoria);
                 minhaLista.add(objeto);
             }
             stmt.close();
@@ -52,13 +55,13 @@ public class AlunoDAO {
     }
 
     /**
-     * Retorna o maior id de um aluno.
+     * Retorna o maior id de um produto.
      */
     public int maiorID() {
         int maiorID = 0;
         try {
             Statement stmt = this.getConexao().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT MAX(id) id FROM tb_alunos");
+            ResultSet res = stmt.executeQuery("SELECT MAX(id) id FROM tb_produtos");
             res.next();
             maiorID = res.getInt("id");
             stmt.close();
@@ -105,18 +108,21 @@ public class AlunoDAO {
     }
 
     /**
-     * Cadastra um novo aluno.
+     * Cadastra um novo produto.
      */
-    public boolean insertAlunoBD(Produto objeto) {
-        String sql = "INSERT INTO tb_alunos(id,nome,idade,curso,fase) VALUES(?,?,?,?,?)";
+    public boolean insertProdutoBD(Produto objeto) {
+        String sql = "INSERT INTO tb_produtos(id,nome,preco,unidade,quantidade,quantidade_minima,quantidade_maxima,categoria) VALUES(?,?,?,?,?,?,?,?)";
         try {
             PreparedStatement stmt = this.getConexao().prepareStatement(sql);
 
             stmt.setInt(1, objeto.getId());
             stmt.setString(2, objeto.getNome());
-            stmt.setInt(3, objeto.getIdade());
-            stmt.setString(4, objeto.getCurso());
-            stmt.setInt(5, objeto.getFase());
+            stmt.setDouble(3, objeto.getPreco());
+            stmt.setString(4, objeto.getUnidade());
+            stmt.setInt(5, objeto.getQuantidade());
+            stmt.setInt(6, objeto.getQuantidadeMinima());
+            stmt.setInt(7, objeto.getQuantidadeMaxima());
+            stmt.setInt(8, objeto.getCategoria());
 
             stmt.execute();
             stmt.close();
@@ -129,12 +135,12 @@ public class AlunoDAO {
     }
 
     /**
-     * Deleta um aluno específico pelo seu campo ID
+     * Deleta um produto específico pelo seu campo ID
      */
-    public boolean deleteAlunoBD(int id) {
+    public boolean deleteProdutoBD(int id) {
         try {
             Statement stmt = this.getConexao().createStatement();
-            stmt.executeUpdate("DELETE FROM tb_alunos WHERE id = " + id);
+            stmt.executeUpdate("DELETE FROM tb_produtos WHERE id = " + id);
             stmt.close();
 
         } catch (SQLException erro) {
@@ -144,20 +150,25 @@ public class AlunoDAO {
     }
 
     /**
-     * Edita um aluno específico pelo seu campo ID
+     * Edita um produto específico pelo seu campo ID
      */
-    public boolean updateAlunoBD(Produto objeto) {
+    public boolean updateProdutoBD(Produto objeto) {
 
-        String sql = "UPDATE tb_alunos set nome = ? ,idade = ? ,curso = ? ,fase = ? WHERE id = ?";
+        String sql = "UPDATE tb_produtos set nome = ? ,preco = ? ,unidade = ? ,quantidade = ? ,quantidade_minima = ? ,quantidade_maxima = ?, id_categoria = ? WHERE id = ?";
 
         try {
             PreparedStatement stmt = this.getConexao().prepareStatement(sql);
 
-            stmt.setString(1, objeto.getNome());
-            stmt.setInt(2, objeto.getIdade());
-            stmt.setString(3, objeto.getCurso());
-            stmt.setInt(4, objeto.getFase());
-            stmt.setInt(5, objeto.getId());
+            stmt.setString(2, objeto.getNome());
+            stmt.setDouble(3, objeto.getPreco());
+            
+            stmt.setString(4, objeto.getUnidade());
+            stmt.setInt(5, objeto.getQuantidade());
+            stmt.setInt(6, objeto.getQuantidadeMinima());
+            stmt.setInt(7, objeto.getQuantidadeMaxima());
+            
+            
+            stmt.setInt(8, objeto.getCategoria());
 
             stmt.execute();
             stmt.close();
@@ -171,26 +182,40 @@ public class AlunoDAO {
     }
 
     /**
-     * Carrega um aluno pelo ID
+     * Carrega um produto pelo ID
      */
-    public Produto carregaAluno(int id) {
-        Produto objeto = new Produto();
-        objeto.setId(id);
+    public Produto carregaProduto(int id) {
+
+        String nome = "";
+        double preco = 0.0;
+        String unidade = "";
+        int quantidade = 0;
+        int quantidadeMinima = 0;
+        int quantidadeMaxima = 0;
+        int categoriaId = 0;
+        
         try {
             Statement stmt = this.getConexao().createStatement();
 
-            ResultSet res = stmt.executeQuery("SELECT * FROM tb_alunos WHERE id = " + id);
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_produtos WHERE id = " + id);
             res.next();
 
-            objeto.setNome(res.getString("nome"));
-            objeto.setIdade(res.getInt("idade"));
-            objeto.setCurso(res.getString("curso"));
-            objeto.setFase(res.getInt("fase"));
+            nome = res.getString("nome");
+            preco = res.getDouble("preco");
+            unidade = res.getString("unidade");
+            quantidade = res.getInt("quantidade");
+            quantidadeMinima = res.getInt("quantidade_minima");
+            quantidadeMaxima  = res.getInt("quantidade_maxima");
+            categoriaId = res.getInt("id_categoria");
 
             stmt.close();
         } catch (SQLException erro) {
             System.out.println("Erro:" + erro);
         }
+        CategoriaDAO categoriaDao = new CategoriaDAO();
+        Categoria categoria = new Categoria();
+        categoria = categoriaDao.carregaCategoria(categoriaId);
+        Produto objeto = new Produto(id, nome, preco, unidade, quantidade, quantidadeMinima, quantidadeMaxima, categoria);
         return objeto;
     }
 }

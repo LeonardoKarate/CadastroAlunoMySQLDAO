e24a55ac97765385ce3a3031da79f147768b4172
LeonardoKarate <leonardokarate95@hotmail.com>
2025-06-04 21:26:52 +0100

    cria classe CategoriaComContagem

---
+++ src/main/java/modelo/CategoriaComContagem.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author leonardoNunes
+ */
+public class CategoriaComContagem {
+    private String nome;
+    private int total;
+    
+    public CategoriaComContagem(String nome, int total) {
+        this.nome = nome;
+        this.total = total;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the total
+     */
+    public int getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    
+}
